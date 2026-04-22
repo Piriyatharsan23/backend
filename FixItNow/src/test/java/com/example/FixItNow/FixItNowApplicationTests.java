c6a1bc089e8b6567729132c67f3059546bf5a121
janindusathsara <janindusathsara1111@gmail.com>
2026-04-22 21:08:29 +0530

    feat: initialize Spring Boot backend for FixItNow

---
+++ FixItNow/src/test/java/com/example/FixItNow/FixItNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FixItNow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixItNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
